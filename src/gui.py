e5f121d6a07cabb58013064aa3e3a5c605b15c74
remiscarlet <w0lf@ahodomo.net>
2014-06-03 21:54:16 -0400

    Fixed the black box where terrain info should be
    ALPHA SURFACES AHAHA I'm an idiot.

--- src/gui.py
+++ src/gui.py
@@ -6,10 +6,12 @@ class GameInfoPanel3(object):
 	def __init__(self):
 		self.fullSurf = pygame.Surface((128,128))
 		self.rect = (256,0,128,128)
-		self.borderTemplate = pygame.Surface((128,128))
+		#Make sure to render a blank/alpha surface
+		self.borderTemplate = pygame.Surface((128,128), pygame.SRCALPHA, 32).convert_alpha()
 		pygame.draw.rect(self.borderTemplate, (0,0,0), (0,0,128,20), 1)
 		pygame.draw.rect(self.borderTemplate, (0,0,0), (0,0,128,128), 1)
 		self.titleSurf = pygame.Surface((128,20))
+		self.titleSurf.fill((255,255,255))
 		self.titleSurfRect = (0,0,128,20)
 		self.imageSurf = pygame.Surface((128,108))
 		self.imageSurfRect = (0,20,128,108)
@@ -18,14 +20,16 @@ class GameInfoPanel3(object):
 	#TileType should be all undercase name of the tile
 	#Surface should always be the gameInfoPanel3, basically.
 	def update(self, tileType, this):
-		tempText = this.dialogueFont.render(tileType, True, (0,0,0))
+		tempText = this.dialogueFont.render(tileType.capitalize(), True, (0,0,0))
 		width = this.dialogueFont.size(tileType)[0]
 		padding = (128-width)/2
+		print padding
 		self.titleSurf.blit(tempText, (padding,0))
-		self.imageSurf.blit(eval("this.tiles."+tileType), (0,20))
+		self.imageSurf.blit(eval("this.tiles."+tileType), (0,0))
 		self.drawPanel()
 
 	def drawPanel(self):
 		self.fullSurf.blit(self.titleSurf, self.titleSurfRect)
 		self.fullSurf.blit(self.imageSurf, self.imageSurfRect)
 		self.fullSurf.blit(self.borderTemplate,(0,0))
+		print "a"
