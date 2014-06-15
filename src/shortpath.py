df7e7bccd572d7865a20a8c0fa3ad3b92f097b94
Ippikiryu <Ippikiryu@hotmail.com>
2014-06-14 23:54:31 -0700

    just changing names
    apparently you have to commit to sync

--- src/shortpath.py
+++ src/shortpath.py
@@ -1,4 +1,4 @@
-def shortPath(terrain, src, dest, max):
+def shortPath(terrain, src, dest, spd):
 	path = []
 	distMap = []
 	rows = len(terrain)
@@ -14,12 +14,17 @@ def shortPath(terrain, src, dest, max):
 
 # Recursively call minNode on each surrounding undistMap (distMap starts as a 2d array initialized to 255 which are then set to the lowest path to given tile)
 
-def minNode(terrain, distMap, src, dest, max, path, rows, cols):
+def minNode(terrain, distMap, src, dest, spd, rows, cols):
 	# Base case
+	# !! Doesn't handle if direct path is not optimal. To be fixed later.
 	x_dist = abs(src[0] - dest[0]) 
 	y_dist = abs(src[1] - dest[1])
 	if (x_dist == 1 and y_dist == 0) or (x_dist == 0 and y_dist == 1): # If src and dest are adjacent
 		distMap[dest[1]][dest[0]] = terrain[dest[1]][dest[0]] # distMap at dest becomes terrain at dest
+
+	# Upper edge (y - 1)
+	if dest[1] > 0 and distMap[dest[1] - 1][dest[0]] == 255 and terrain[dest[1] - 1][dest[0]] != -1:
+		upper = minNode(terrain, distMap, src, (dest[0], dest[1] - 1), spd, rows, cols)
 
 
 terrain = [[1, 1, 1, 1, 1, 1, 1, 1], 
